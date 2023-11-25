59a43e1654169c9b1194805fcdad0fee4c8e6c62
yilan0916 <yilan0916@gmail.com>
2023-11-26 00:33:43 +0800

    项目初次构建，完成common模块的代码生成器

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # awesome-java
+
+## 记录和总结自己的coding之路
---
+++ awesome-java-system/src/main/java/com/yilan/awesome/Application.java
@@ -0,0 +1,16 @@
+package com.yilan.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author： yilan0916
+ * @date : 2023/11/25
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
